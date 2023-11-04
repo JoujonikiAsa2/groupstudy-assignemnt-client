43e3481b28d0c10c91f04f3a495d979bf4ebddd6
JoujonikiAsa2 <joujonikiasaroy@gmail.com>
2023-11-04 22:01:06 +0600

    Added the code for connection with mongodb

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
+const cors = require('cors')
+require('dotenv').config()
 const port = process.env.PORT || 5000
+
+// midlewares
+app.use(cors())
+app.use(express.json())
+
+
+// database connections
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ghkhwep.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } 
+  catch{
+    console.log("Found an error, Please check")
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`This app listening on port ${port}!`))
