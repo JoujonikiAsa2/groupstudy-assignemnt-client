c7b9c1c1f405306e41c4d8db3db2af2568fb570f
JoujonikiAsa2 <joujonikiasaroy@gmail.com>
2023-11-09 15:38:25 +0600

    Added jwtauthorization

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
 const express = require('express')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app = express()
 const cors = require('cors')
+const jwt = require("jsonwebtoken")
+const cookieParser = require('cookie-parser')
+const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
 // midlewares
-app.use(cors())
-app.use(express.json())
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}))
 
+app.use(express.json())
+app.use(cookieParser())
+
+
+// Create custom middlewares
+
+const logger = (req, res, next) => {
+  console.log(req.hostname, req.method, req.url)
+  next()
+}
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log('valueof tocken in middlewares', token)
+  try {
+    if (!token) {
+      return res.status(401).send({ message: 'unauthorized' })
+    }
+    jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, (err, decoded) => {
+      if (err) {
+        return res.status(403).send({ message: 'Page Not Found' })
+      }
+      console.log("Token Value", decoded)
+      req.user = decoded
+    })
+    next()
+  }
+  catch {
+    console.log("Invalied Token")
+    return res.status(401).send({ message: 'unauthorized' })
+  }
+}
 
 // database connections
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ghkhwep.mongodb.net/?retryWrites=true&w=majority`;
@@ -35,13 +70,13 @@ async function run() {
 
     // Submission
 
-    app.get("/submissions", async (req, res) => {
+    app.get("/submissions",logger, verifyToken, async (req, res) => {
       const assignments = submittedAssignmentCollections.find()
       const result = await assignments.toArray()
       res.send(result)
     })
 
-    app.get("/submissions/:id", async (req, res) => {
+    app.get("/submissions/:id",  logger,verifyToken, async (req, res) => {
       try {
         const id = req.params.id
         const query = { _id: new ObjectId(id) }
@@ -52,14 +87,14 @@ async function run() {
         console.log("error")
       }
     })
-    app.get("/submissions/user/:email", async (req, res) => {
+    app.get("/submissions/user/:email", logger,verifyToken, async (req, res) => {
       const email = req.params.email
       const query = { email: email }
       const assignments = submittedAssignmentCollections.find(query)
       const result = await assignments.toArray()
       res.send(result)
     })
-    app.get("/submissions/statusCode/:status", async (req, res) => {
+    app.get("/submissions/statusCode/:status", logger,verifyToken, async (req, res) => {
       try {
         const status = req.params.status
         const query = { status: status }
@@ -78,10 +113,11 @@ async function run() {
       const assignments = assignmentsCollections.find()
       const result = await assignments.toArray()
       res.send(result)
+      console.log(process.env.ACCESS_SECRET_TOKEN)
     })
 
 
-    app.get("/assignments/:id", async (req, res) => {
+    app.get("/assignments/:id", logger,verifyToken, async (req, res) => {
       try {
         const id = req.params.id
         const query = { _id: new ObjectId(id) }
@@ -94,7 +130,7 @@ async function run() {
       }
     })
 
-    app.get("/assignments/emailAdd/:email", async (req, res) => {
+    app.get("/assignments/emailAdd/:email", logger,verifyToken, async (req, res) => {
       const email = req.params.email
       const query = { creatorEmail: email }
       const assignments = assignmentsCollections.find(query)
@@ -116,6 +152,27 @@ async function run() {
       }
     })
 
+    // generate jwt for secure the api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body
+      console.log(user)
+      const token = jwt.sign(user, process.env.ACCESS_SECRET_TOKEN, { expiresIn: '3h' })
+
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: false,
+          // sameSite: 'none'
+        })
+        .send({ success: true })
+    })
+
+    app.post('/logout', async (req, res) => {
+      const user = req.body;
+      console.log("Loggin out user:", user)
+      res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+  })
+
     // Assignment
     app.post("/assignments", async (req, res) => {
       const assignments = req.body
@@ -136,7 +193,7 @@ async function run() {
         const id = req.params.id
         const query = { _id: new ObjectId(id) }
         const newAssignment = req.body
-        const option = {upsert: true}
+        const option = { upsert: true }
         const updatedAssignment = {
           $set: {
             title: newAssignment.title,
@@ -157,7 +214,7 @@ async function run() {
         error => console.log(error)
       }
     })
-    
+
     app.patch("/submissions/:id", async (req, res) => {
       try {
         const id = req.params.id
